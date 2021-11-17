3b5fe650ed4a7af658d3507ab2ff77239739178f
Daniel-Lima14 <91990139+Daniel-Lima14@users.noreply.github.com>
2021-11-16 21:27:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function operacoes (a,b){
+     alert(`A multiplicaçao entre os dois numeros e ${a*b}`)
+     
+}
+
+a = parseInt(prompt("Digite um numero"))
+b = parseInt(prompt("Digite outro numero"))
+
+operacoes(a,b)
+
